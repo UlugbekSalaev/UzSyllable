fb78b2446196b4e28275b1700a3c57b42e0e79a5
ulugbek0302 <ulugbek0302@gmail.com>
2023-05-10 11:08:56 +0500

    count of syllables

--- Training_models/RandomForest.py
+++ Training_models/RandomForest.py
@@ -1,19 +1,26 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-# Load the syllabification dataset
-df = pd.read_csv('../dataset/dataset.csv')
+# Load the dataset
+data = pd.read_csv('../dataset/binary.csv')
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(df.drop('syllables', axis=1), df['syllables'], test_size=0.2, random_state=42)
+# Preprocess the data
+X = data['x'].apply(lambda x: [int(d) for d in str(x)]).tolist()
+y = data['y'].apply(lambda x: [int(d) for d in str(x).replace('-', '')]).tolist()
 
-# Train a Random Forest classifier
-rf = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
-rf.fit(X_train, y_train)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Evaluate the performance of the classifier on the testing set
-y_pred = rf.predict(X_test)
+# Train the Random Forest Classifier
+rfc = RandomForestClassifier(n_estimators=100)
+rfc.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = rfc.predict(X_test)
+
+# Calculate the accuracy of the model
 accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
+
+print(f"Accuracy: {accuracy}")
