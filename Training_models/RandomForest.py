0613c37460955a23137c47b7a0eb179b38c76c96
ulugbek0302 <ulugbek0302@gmail.com>
2023-05-10 12:54:34 +0500

    Binary

--- Training_models/RandomForest.py
+++ Training_models/RandomForest.py
@@ -1,26 +1,40 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
-# Load the dataset
-data = pd.read_csv('../dataset/binary.csv')
+# Load the dataset from a CSV file
+df = pd.read_csv('../dataset/binary.csv')
 
-# Preprocess the data
-X = data['x'].apply(lambda x: [int(d) for d in str(x)]).tolist()
-y = data['y'].apply(lambda x: [int(d) for d in str(x).replace('-', '')]).tolist()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df['x'], df['y'], test_size=0.2, random_state=42)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# Define a function to convert a string of * and # into a feature vector
+def str_to_vec(s):
+    return [1 if c == '*' else 0 if c == "#" else "-" for c in s]
 
-# Train the Random Forest Classifier
-rfc = RandomForestClassifier(n_estimators=100)
-rfc.fit(X_train, y_train)
+# Convert the training and testing set strings to feature vectors
+X_train = [str_to_vec(s) for s in X_train]
+X_test = [str_to_vec(s) for s in X_test]
 
-# Make predictions on the test set
-y_pred = rfc.predict(X_test)
+# Define a function to convert a string of *, #, and - into a label vector
+def str_to_label(s):
+    return [c for c in s]
 
-# Calculate the accuracy of the model
-accuracy = accuracy_score(y_test, y_pred)
+# Convert the training and testing set strings to label vectors
+y_train = [str_to_label(s) for s in y_train]
+y_test = [str_to_label(s) for s in y_test]
 
-print(f"Accuracy: {accuracy}")
+y_train = [str_to_vec(s) for s in y_train]
+print((y_train))
+
+# Define the decision tree classifier and train it on the training set
+clf = DecisionTreeClassifier(random_state=42)
+clf.fit(X_train, y_train)
+
+# Use the trained model to make predictions on the testing set
+y_pred = clf.predict(X_test)
+
+# Compute the accuracy of the model
+acc = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {acc:.2f}")
